4b7886e17a6e7e6a7c35564b78e01f44621eb636
Yinkubell <bsuliat20@yahoo.com>
2023-02-20 23:39:43 +0100

    my commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-my work on operators control flow
+/List the symbols for each of the below Javascript / Arithmetic Operators Assignment Operators Comparison Operators Logical Operators Bitwise Operators */
+/For each JavaScript Operator, write 2 examples./ 
+Arithmetic operators
+  +, -, *, /
+  Eg-  a = 6;
+     b = 2;
+
+    (a + b)
+    (a - b)
+
+ Assignment operators
+    =, +=, -=, /=, *=
+    Eg-a = 4
+       b = a +2
+
+ Comparision operators
+    <, >, <=, >=, !+, ==, ===
+    Eg- x = 3;
+    (x > 5)
+    (x <= 4)
+ Logical operators
+    &&, ||, !
+    Eg- eligibleForLoan = highIncome && goodCreditScore;
+    eligibleForLoan = highIncome || goodCreditScore;
+
+  /Number 4/ for (let i=1; i<20; i+= 7){ console.log(i) }
+
+Result 1 8 15
+
+
+    
+
+
 
---
+++ index.js
@@ -0,0 +1,37 @@
+/*number 3*/
+
+let science = "physics,chemistry, biology, TechnicalDrawing";
+
+let Socialscience = "Accounting, Commerce, Marketing, Geography";
+
+let Art = "Government, Economics, Literature";
+
+let Generalstudies = "Englsih, Maths"
+
+let department= Art
+
+if(department = Art){
+    console.log("These are the courses for Art: "+ Art+ "" +  Generalstudies);
+}else if (department = science){
+    console.log("These are the courses for science:" + science +"" + Generalstudies);
+}else if (department = Socialscience){
+    console.log("These are the courses for Socialscience:" + Socialscience + "" + Generalstudies );
+}else{
+    console.log(Generalstudies)
+}
+
+console.log(department);
+  
+
+
+/*number 5*/
+
+let num = 20;
+
+2**3;
+2**4;
+
+let pwr = 2**4;
+console.log("The number" + pwr + "is the power of 2 nearest to" + num);
+
+
